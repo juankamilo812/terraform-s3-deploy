432863dcd2ac8c69c7abc44581d7a5ecdcac1890
Juan Camilo Garcia Hurtado <juankamilo812@example.com>
2025-06-09 20:11:10 -0500

    fix: prevent AWS profile from being used in GitHub Actions

--- provider.tf
+++ provider.tf
@@ -1,6 +1,14 @@
+# provider "aws" {
+ # region = "us-east-1"
+
+  # Esta condición solo aplica el profile si no está vacío
+  # profile = length(var.profile) > 0 ? var.profile : null
+#}
+
+
 provider "aws" {
   region = "us-east-1"
 
-  # Esta condición solo aplica el profile si no está vacío
-  profile = length(var.profile) > 0 ? var.profile : null
+  # Usa el perfil local solo si existe (no en GitHub Actions)
+  profile = (try(var.profile, "") != "") ? var.profile : null
 }
