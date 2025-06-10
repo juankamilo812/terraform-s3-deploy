451b9aabfb2a1f118e30467766750b36e16b97ef
Juan Camilo Garcia Hurtado <juankamilo812@example.com>
2025-06-09 20:31:13 -0500

    fix: apply variable

--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@ variable "bucket_name" {
   description = "Nombre del bucket S3"
   type        = string
 }
+
+variable "profile" {
+  description = "Perfil AWS local (opcional para desarrollo)"
+  type        = string
+  default     = ""
+}
