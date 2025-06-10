8dce29a1ac3b367415f8eb73943bdfe5d731cd11
Juan Camilo Garcia Hurtado <juankamilo812@example.com>
2025-06-09 22:24:01 -0500

    apply3 s3 backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "tfstate812"
-    key    = "terraform-s3-deploy/terraform.tfstate"
-    region = "us-east-1"
+    bucket  = "tfstate812"
+    key     = "terraform-s3-deploy/terraform.tfstate"
+    region  = "us-east-1"
     encrypt = true
   }
 }
