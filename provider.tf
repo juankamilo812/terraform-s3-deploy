21acbec8e900414ffe807104fb2354fad1b3bd91
Juan Camilo Garcia Hurtado <juankamilo812@example.com>
2025-06-09 20:14:24 -0500

    fix: reformat provider.tf with correct permissions

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 # provider "aws" {
- # region = "us-east-1"
+# region = "us-east-1"
 
-  # Esta condición solo aplica el profile si no está vacío
-  # profile = length(var.profile) > 0 ? var.profile : null
+# Esta condición solo aplica el profile si no está vacío
+# profile = length(var.profile) > 0 ? var.profile : null
 #}
 
 
